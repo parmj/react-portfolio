cdf8d3743136b18c5c88a604e8fe93fc15adc26d
ParmJohal <parmj@sfu.ca>
2020-04-12 18:47:21 -0700

    Updates

---
+++ precache-manifest.3b8e9086f4aa3d8dede1662e68582a66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feda1eaa48a9be3b19bb98b8328c88de",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "425f7e303da14468285c",
+    "url": "/react-portfolio/static/css/2.735a280d.chunk.css"
+  },
+  {
+    "revision": "d2fddb200cc0013becfa",
+    "url": "/react-portfolio/static/css/main.ce8c1d87.chunk.css"
+  },
+  {
+    "revision": "425f7e303da14468285c",
+    "url": "/react-portfolio/static/js/2.55ac42b5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-portfolio/static/js/2.55ac42b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2fddb200cc0013becfa",
+    "url": "/react-portfolio/static/js/main.3b754613.chunk.js"
+  },
+  {
+    "revision": "eff633beff9fe05cfab3",
+    "url": "/react-portfolio/static/js/runtime-main.6b1a3ce1.js"
+  }
+]);
